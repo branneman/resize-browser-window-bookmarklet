b37ac7ffea2ea7a99a580071b01e4ccd9c9812b3
Bran van der Meer <branmovic@gmail.com>
2011-08-09 11:42:42 +0200

    cleaned tabs/spaces use, all spaces now

--- resize-browser-window.js
+++ resize-browser-window.js
@@ -1,13 +1,13 @@
 (function(d){
 
-	// Dont open another resizer div when one's already open
-	if (d.getElementById('bm_resizer')) {
-		return;
-	}
+    // Dont open another resizer div when one's already open
+    if (d.getElementById('bm_resizer')) {
+        return;
+    }
 
-	// Load Bookmarklet JS
-	var scriptNode = d.createElement('script');
-	scriptNode.src = 'http://branneman.github.com/resize-browser-window-bookmarklet/bookmarklet-resizer.js';
-	d.body.appendChild(scriptNode);
+    // Load Bookmarklet JS
+    var scriptNode = d.createElement('script');
+    scriptNode.src = 'http://branneman.github.com/resize-browser-window-bookmarklet/bookmarklet-resizer.js';
+    d.body.appendChild(scriptNode);
 
 }(document));
